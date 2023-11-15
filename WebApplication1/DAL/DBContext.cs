29eccf0cf74f26ba8988d721b06261c8e1b031a7
rhuan <rhuankevinn11@gmail.com>
2023-11-14 22:25:40 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/DAL/DBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.DAL
+{
+    public class DBContext : DbContext
+    {
+        public DBContext() : base("DBContext")
+        {
+        }
+
+        public DbSet<Aluno>Alunos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<System.Data.Entity.ModelConfiguration.Conventions.PluralizingTableNameConvention>();
+        }
+
+    }
+}
